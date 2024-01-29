580fbf92899599adee1e9d5ce56cc95b254481ec
naresh1-23 <nareshtamang20000@gmail.com>
2024-01-29 19:29:10 +0545

    changes made

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
+from .models import Videos
 
-# Register your models here.
+@register(Videos)
+class UserAdmin(ModelAdmin):
+    list_display = ('name', 'rating', 'created_at', "updated_at")
